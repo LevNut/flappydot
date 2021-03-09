209539a60a9266471197e8cb3bc4fbd4ad36c62c
Jittat Fakcharoenphol <jittat@gmail.com>
2021-03-10 02:11:10 +0700

    Show dot

---
+++ flappydot.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+
+from gamelib import Sprite, GameApp, Text
+
+CANVAS_WIDTH = 800
+CANVAS_HEIGHT = 500
+
+UPDATE_DELAY = 33
+GRAVITY = 1
+
+class Dot(Sprite):
+    pass
+
+
+class MonkeyGame(GameApp):
+    def create_sprites(self):
+        self.dot = Dot(self, 'images/dot.png', CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2)
+
+        self.elements.append(self.dot)
+
+    def init_game(self):
+        self.create_sprites()
+
+    def on_key_pressed(self, event):
+        pass
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Monkey Banana Game")
+ 
+    # do not allow window resizing
+    root.resizable(False, False)
+    app = MonkeyGame(root, CANVAS_WIDTH, CANVAS_HEIGHT, UPDATE_DELAY)
+    app.start()
+    root.mainloop()
